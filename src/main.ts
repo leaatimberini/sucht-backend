421b71b1d4df9776f141c855890e780120a2bb4b
Leandro <timberinil@gmail.com>
2025-07-28 17:21:32 -0300

    Cambiamos a Cloudinary service 6

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,28 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   process.env.TZ = 'America/Argentina/Buenos_Aires';
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // CORRECCIÓN: Configuramos las rutas de archivos estáticos ANTES del prefijo global.
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+  
   app.setGlobalPrefix('api');
+
   app.enableCors({
-    origin: [ 'http://localhost:3000', 'https://sucht.com.ar', 'http://sucht.com.ar' ],
+    origin: [
+      'http://localhost:3000',
+      'https://sucht.com.ar',
+      'http://sucht.com.ar',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
-  // Servimos la carpeta principal de uploads Y la nueva de perfiles
-  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-    prefix: '/uploads/',
-  });
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
 
   const port = process.env.APP_PORT || 5000;
   await app.listen(port);
