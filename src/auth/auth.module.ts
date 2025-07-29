7f6aed79b224e989fa42adb4a38c1b26743fb77c
Leandro <timberinil@gmail.com>
2025-07-29 12:16:38 -0300

    Base 17

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'; 
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
@@ -6,7 +6,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
-import { JwtStrategy } from './strategies/jwt.strategy'; // <-- IMPORTAR
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { MailModule } from 'src/mail/mail.module'; // <-- IMPORTAR MailModule
 
 @Module({
   imports: [
@@ -20,8 +21,9 @@ import { JwtStrategy } from './strategies/jwt.strategy'; // <-- IMPORTAR
         signOptions: { expiresIn: '24h' },
       }),
     }),
+    MailModule, // <-- AGREGAR MailModule aquí
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy], // <-- AÑADIR JwtStrategy
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
