9267cf8517f1b6d166d5786b36d4d13c59305d4e
Leandro <timberinil@gmail.com>
2025-07-29 01:03:59 -0300

    Base 10

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { CloudinaryModule } from 'src/cloudinary/cloudinary.module'; // 1. IMPORTAR
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
+import { ConfigurationModule } from 'src/configuration/configuration.module'; // <-- 1. IMPORTAR
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
-    CloudinaryModule, // 2. AÑADIR A LOS IMPORTS
+    CloudinaryModule,
+    ConfigurationModule, // <-- 2. AÑADIR A LOS IMPORTS
   ],
   providers: [UsersService],
   exports: [UsersService],
