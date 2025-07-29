8e6e30025ab584701b480f076e1c11ebe9b2c1d8
Leandro <timberinil@gmail.com>
2025-07-29 01:01:24 -0300

    Base 9

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
-import { UsersModule } from 'src/users/users.module'; 
+import { ConfigModule } from '@nestjs/config';
+import { TicketsModule } from 'src/tickets/tickets.module';
+import { UsersModule } from 'src/users/users.module';
+import { TicketTiersModule } from 'src/ticket-tiers/ticket-tiers.module';
+
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule,
+    TicketsModule,
+    UsersModule,
+    TicketTiersModule,
+  ],
+  controllers: [PaymentsController],
   providers: [PaymentsService],
-  controllers: [PaymentsController]
 })
 export class PaymentsModule {}
