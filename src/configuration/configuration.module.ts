6df03aff4faa09ff86937c179dc8c0da1e84dd41
Leandro <timberinil@gmail.com>
2025-07-29 01:10:16 -0300

    Base 12

--- src/configuration/configuration.module.ts
+++ src/configuration/configuration.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigurationService } from './configuration.service';
-import { ConfigurationController } from './configuration.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Configuration } from './configuration.entity';
+import { ConfigurationService } from './configuration.service';
+import { ConfigurationController } from './configuration.controller';
 
 @Module({
-  // --- AÑADIDO: Importamos TypeOrmModule para la entidad Configuration ---
   imports: [TypeOrmModule.forFeature([Configuration])],
-  controllers: [ConfigurationController],
   providers: [ConfigurationService],
+  controllers: [ConfigurationController],
+  // --- CORRECCIÓN: Añadimos 'exports' para que otros módulos puedan usar el servicio ---
+  exports: [ConfigurationService],
 })
 export class ConfigurationModule {}
