7ac96d5d7039313f70e65e1843a98d6920ba86d4
Leandro <timberinil@gmail.com>
2025-08-02 01:54:25 -0300

    add: notificaciones 1

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { PushSubscription } from './entities/subscription.entity';
+import { Notification } from './entities/notification.entity'; // 1. IMPORTAR LA NUEVA ENTIDAD
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PushSubscription])],
+  // 2. AÑADIR LA ENTIDAD AL REGISTRO DE TYPEORM
+  imports: [TypeOrmModule.forFeature([PushSubscription, Notification])],
   controllers: [NotificationsController],
   providers: [NotificationsService],
-  exports: [NotificationsService], // Exportamos el servicio para que otros módulos lo usen
+  exports: [NotificationsService],
 })
 export class NotificationsModule {}
