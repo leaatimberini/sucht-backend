82fe66a926037737c80cd024e53f5382ec1be858
Leandro <timberinil@gmail.com>
2025-08-07 00:50:53 -0300

    Version 2.7.2 backend

--- src/store/dto/create-product.dto.ts
+++ src/store/dto/create-product.dto.ts
@@ -1,28 +1,34 @@
 import { IsString, IsNotEmpty, IsNumber, Min, IsOptional, IsUrl, IsBoolean, IsInt } from 'class-validator';
 
 export class CreateProductDto {
-  @IsString()
-  @IsNotEmpty()
-  name: string;
+  @IsString()
+  @IsNotEmpty()
+  name: string;
 
-  @IsString()
-  @IsOptional()
-  description?: string;
+  @IsString()
+  @IsOptional()
+  description?: string;
 
+  @IsNumber()
+  @Min(0)
+  price: number;
+
+  // ===== NUEVO CAMPO PARA EL PRECIO ORIGINAL =====
   @IsNumber()
   @Min(0)
-  price: number;
-
-  @IsInt()
-  @Min(0)
   @IsOptional()
-  stock?: number;
+  originalPrice?: number;
 
-  @IsUrl()
-  @IsOptional()
-  imageUrl?: string;
+  @IsInt()
+  @Min(0)
+  @IsOptional()
+  stock?: number;
 
-  @IsBoolean()
-  @IsOptional()
-  isActive?: boolean;
+  @IsUrl()
+  @IsOptional()
+  imageUrl?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isActive?: boolean;
 }
