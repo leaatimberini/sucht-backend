c05b9dab7904014850459e82bc474b6ec0db878f
Leandro <timberinil@gmail.com>
2025-08-13 11:35:51 -0300

    Version 3.3.3 backend

--- src/ticket-tiers/ticket-tiers.module.ts
+++ src/ticket-tiers/ticket-tiers.module.ts
@@ -1,33 +1,16 @@
 import { Module } from '@nestjs/common';
-import { OwnerInvitationService } from './owner-invitations.service';
-import { OwnerInvitationController } from './owner-invitations.controller';
-import { UsersModule } from '../users/users.module';
-import { EventsModule } from '../events/events.module';
-import { TicketTiersModule } from '../ticket-tiers/ticket-tiers.module';
-import { TicketsModule } from '../tickets/tickets.module';
-import { MailModule } from '../mail/mail.module';
-import { ConfigurationModule } from '../configuration/configuration.module';
-import { StoreModule } from '../store/store.module';
+import { TicketTiersService } from './ticket-tiers.service';
+// Se importa el nuevo controlador
+import { TicketTiersController, RootTicketTiersController } from './ticket-tiers.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Ticket } from '../tickets/ticket.entity';
-import { ProductPurchase } from '../store/product-purchase.entity';
+import { TicketTier } from './ticket-tier.entity';
+import { EventsModule } from 'src/events/events.module';
 
 @Module({
-  imports: [
-    // --- LÍNEA AÑADIDA ---
-    // Registramos las entidades que el servicio necesita inyectar directamente.
-    TypeOrmModule.forFeature([Ticket, ProductPurchase]),
-    
-    // Importamos todos los módulos cuyos servicios vamos a orquestar
-    UsersModule,
-    EventsModule,
-    TicketTiersModule,
-    TicketsModule,
-    StoreModule,
-    MailModule,
-    ConfigurationModule,
-  ],
-  controllers: [OwnerInvitationController],
-  providers: [OwnerInvitationService],
+  imports: [TypeOrmModule.forFeature([TicketTier]), EventsModule],
+  // Se añade el nuevo controlador a la lista
+  controllers: [TicketTiersController, RootTicketTiersController],
+  providers: [TicketTiersService],
+  exports: [TicketTiersService],
 })
-export class OwnerInvitationModule {}
+export class TicketTiersModule {}
