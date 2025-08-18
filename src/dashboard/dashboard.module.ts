dda9dd96e051501ac98e8ed83f179171830e9817
Leandro <timberinil@gmail.com>
2025-08-18 01:05:25 -0300

    Version 3.6.5 backend

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Ticket } from 'src/tickets/ticket.entity';
 import { Event } from 'src/events/event.entity';
-import { User } from 'src/users/user.entity'; // 1. IMPORTAR
+import { User } from 'src/users/user.entity';
+import { TicketsModule } from 'src/tickets/tickets.module'; // 1. Importar TicketsModule
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ticket, Event, User])], // 2. AÑADIR User
+  imports: [
+    TypeOrmModule.forFeature([Ticket, Event, User]),
+    // 2. Añadir TicketsModule para que DashboardService pueda usar TicketsService
+    forwardRef(() => TicketsModule),
+  ],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
