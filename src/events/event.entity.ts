33e61fada8c4e29db14cfd652cf14941553a9ef8
Leandro <timberinil@gmail.com>
2025-08-18 12:31:33 -0300

    Version 3.6.8 backend

--- src/events/event.entity.ts
+++ src/events/event.entity.ts
@@ -23,10 +23,11 @@ export class Event {
   @Column()
   location: string;
 
-  @Column({ type: 'timestamp with time zone' }) // Se recomienda usar 'with time zone'
+  // Se mantiene el tipo original para no romper la compatibilidad
+  @Column({ type: 'timestamp' })
   startDate: Date;
 
-  @Column({ type: 'timestamp with time zone' })
+  @Column({ type: 'timestamp' })
   endDate: Date;
 
   @Column({ nullable: true })
@@ -35,22 +36,12 @@ export class Event {
   @Column({ type: 'timestamp', nullable: true })
   confirmationSentAt: Date | null;
 
-  // --- NUEVOS CAMPOS PARA PUBLICACIÓN PROGRAMADA ---
-
-  /**
-   * Si es 'true', el evento es visible para el público en general.
-   */
+  // --- NUEVOS CAMPOS ---
   @Column({ type: 'boolean', default: false })
   isPublished: boolean;
 
-  /**
-   * La fecha y hora en que el evento se publicará automáticamente.
-   * Si es nulo, se debe publicar manualmente.
-   */
   @Column({ type: 'timestamp with time zone', nullable: true })
   publishAt: Date | null;
-
-  // --- FIN DE NUEVOS CAMPOS ---
 
   @OneToMany(() => Ticket, (ticket) => ticket.event, {
     cascade: true,
