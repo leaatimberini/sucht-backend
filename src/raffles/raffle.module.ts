8b36e531d7cd272397c879f2f404a077ffeb1cbd
Leandro <timberinil@gmail.com>
2025-08-22 06:19:28 -0300

    Version 4.3.0 backend

--- src/raffles/raffle.module.ts
+++ src/raffles/raffle.module.ts
@@ -1,30 +1,26 @@
-// src/raffles/raffle.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RaffleWinner } from './raffle-winner.entity';
 import { RaffleService } from './raffle.service';
+import { RaffleController } from './raffle.controller';
+import { Raffle } from './raffle.entity';
+import { RaffleWinner } from './raffle-winner.entity';
+import { RafflePrize } from './raffle-prize.entity';
 import { EventsModule } from '../events/events.module';
 import { TicketsModule } from '../tickets/tickets.module';
-import { ConfigurationModule } from '../configuration/configuration.module';
 import { StoreModule } from '../store/store.module';
 import { UsersModule } from '../users/users.module';
 import { NotificationsModule } from '../notifications/notifications.module';
-import { RaffleController } from './raffle.controller';
 
 @Module({
   imports: [
-    // Registramos la nueva entidad en TypeORM
-    TypeOrmModule.forFeature([RaffleWinner]),
-    // Importamos todos los módulos cuyos servicios vamos a necesitar
+    TypeOrmModule.forFeature([Raffle, RaffleWinner, RafflePrize]),
     EventsModule,
     TicketsModule,
-    ConfigurationModule,
     StoreModule,
     UsersModule,
     NotificationsModule,
   ],
-  providers: [RaffleService],
-  // Añadiremos el controlador en el siguiente paso
   controllers: [RaffleController],
+  providers: [RaffleService],
 })
 export class RaffleModule {}
